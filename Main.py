69ecbeb6d5c074673d2deb92ae602bf7e880e940
Akanksha Muthe <akankshamuthe@gmail.com>
2024-06-22 18:00:15 +0530

    Create Main.py
    Create a newspaper article object.

---
+++ Main.py
@@ -0,0 +1,20 @@
+import newspaper
+import feedparser
+
+def scrape_news_from_feed(feed_url):
+    articles = []
+    feed = feedparser.parse(feed_url):
+    for entry in feed.entries:
+        article = newspaper.Article(entry.link)
+        
+    #     article.download()
+    #     article.parse()
+
+    #     articles.append({
+    #         'title': article.title,
+    #         'author': article.authors,
+    #         'publish_date': article.publish_date,
+    #         'content': article.text
+    #     })
+    # return articles
+
